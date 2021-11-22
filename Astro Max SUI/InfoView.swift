7ea46f0e04d417b2d2a4d5182aa370ddb03c55fd
Greg Wu <mrgrwu@gmail.com>
2021-11-22 15:09:19 -0800

    Modify dismiss code for InfoView.

--- Astro Max SUI/InfoView.swift
+++ Astro Max SUI/InfoView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct InfoView: View {
-    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         ZStack {
@@ -16,7 +16,7 @@ struct InfoView: View {
             VStack(spacing: 30) {
                 Text("Astrophotographers use the \"500 Rule\" as a way to estimate the maximum shutter speed that can be used to capture the night sky before star trailing becomes noticeable.\n\nDivide 500 (or a lower number rule) by the focal length of your full frame lens (crop factor of 1.0). For crop sensor cameras, the crop factor will be greater than 1.0 in order to scale or zoom up to a full frame equivalent size. Divide again by this value, which will reduce the usable shutter speed. (Crop factor of 1.5 for Nikon APS-C camera, 1.6 for Canon)")
                 Button("Done") {
-                    presentationMode.wrappedValue.dismiss()
+                    dismiss()
                 }
                 .font(.title2)
             }
