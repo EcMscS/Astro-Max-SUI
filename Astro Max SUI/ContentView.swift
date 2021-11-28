8f031bff56c72a5d6ef7783a5e18b9a1778afbf7
Greg Wu <mrgrwu@gmail.com>
2021-11-28 12:26:11 -0800

    Update UI, including UIKit customization for SwiftUI pickers. Incorporate UserDefaults for picker selections, and first time run values for all stored values.

--- Astro Max SUI/ContentView.swift
+++ Astro Max SUI/ContentView.swift
@@ -9,23 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var lensFocalLength = UserDefaults.standard.double(forKey: "LensFocalLength")
-    @State private var cropFactor = UserDefaults.standard.double(forKey: "CropFactor")
     let lowerFocalLimit = 5
-    @State private var upperFocalLimitSelection = 1
-    @State private var ruleSelection = 4
+    @State private var upperFocalLimitSelection = UserDefaults.standard.integer(forKey: "UpperFocalLimitSelection")
+    @State private var cropFactor = UserDefaults.standard.double(forKey: "CropFactor")
+    @State private var ruleSelection = UserDefaults.standard.integer(forKey: "RuleSelection")
     @State private var showingInfoView = false
     
     let upperFocalLimits = [50, 100, 150, 200, 250, 300]
     let rules = [100, 200, 300, 400, 500]
     
+    init() {  // Add UIKit customization for SwiftUI sliders and segmented pickers
+        UISlider.appearance().thumbTintColor = .systemRed
+        UISegmentedControl.appearance().backgroundColor = .systemRed
+        UISegmentedControl.appearance().selectedSegmentTintColor = .black
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor : UIColor.black], for: .normal)
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor : UIColor.systemRed], for: .selected)
+    }
+    
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
+            
             VStack(spacing: 25) {
                 Text("Astro Max")
-                    .font(.largeTitle).fontWeight(.bold)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
                 HStack {
                     Text("Lens Focal Length")
+                        .fontWeight(.medium)
                     Spacer()
                     Text(" \(lensFocalLength, specifier: "%.0f") mm ")
                         .fontWeight(.medium)
@@ -33,7 +45,8 @@ struct ContentView: View {
                         .background(Color.red)
                         .cornerRadius(5)
                 }
-                .font(.title2)
+                    .font(.title2)
+                
                 Slider(value: $lensFocalLength, in: Double(lowerFocalLimit)...Double(upperFocalLimits[upperFocalLimitSelection]), step: 1) {
                     Text("")
                 } minimumValueLabel: {
@@ -44,16 +57,21 @@ struct ContentView: View {
                     UserDefaults.standard.set(self.lensFocalLength, forKey: "LensFocalLength")
                 }
                     .accentColor(.red)
+                
                 Picker("Upper Focal Limit", selection: $upperFocalLimitSelection) {
                     ForEach(0 ..< upperFocalLimits.count) {
                         Text("\(self.upperFocalLimits[$0]) mm")
                     }
                 }
                     .pickerStyle(SegmentedPickerStyle())
-                    .background(Color.red)
                     .cornerRadius(5)
+                    .onChange(of: upperFocalLimitSelection) { _ in
+                        UserDefaults.standard.set(self.upperFocalLimitSelection, forKey: "UpperFocalLimitSelection")
+                    }
+                
                 HStack {
                     Text("Crop Factor")
+                        .fontWeight(.medium)
                     Spacer()
                     Text(" \(cropFactor, specifier: "%.1f") ")
                         .fontWeight(.medium)
@@ -61,7 +79,8 @@ struct ContentView: View {
                         .background(Color.red)
                         .cornerRadius(5)
                 }
-                .font(.title2)
+                    .font(.title2)
+                
                 Slider(value: $cropFactor, in: 1...7, step: 0.1) {
                     Text("")
                 } minimumValueLabel: {
@@ -72,16 +91,21 @@ struct ContentView: View {
                     UserDefaults.standard.set(self.cropFactor, forKey: "CropFactor")
                 }
                     .accentColor(.red)
+                
                 Picker("Calculation Rule", selection: $ruleSelection) {
                     ForEach(0 ..< rules.count) {
                         Text("\(self.rules[$0]) rule")
                     }
                 }
                     .pickerStyle(SegmentedPickerStyle())
-                    .background(Color.red)
                     .cornerRadius(5)
+                    .onChange(of: ruleSelection) { _ in
+                        UserDefaults.standard.set(self.ruleSelection, forKey: "RuleSelection")
+                    }
+                
                 HStack {
                     Text("Max Shutter Speed")
+                        .fontWeight(.medium)
                     Spacer()
                     Text(" \(Double(rules[ruleSelection]) / lensFocalLength / cropFactor, specifier: "%.1f") s ")
                         .fontWeight(.medium)
@@ -89,7 +113,8 @@ struct ContentView: View {
                         .background(Color.red)
                         .cornerRadius(5)
                 }
-                .font(.title2)
+                    .font(.title2)
+                
                 HStack {
                     Text("     ")
                     Spacer()
@@ -98,14 +123,28 @@ struct ContentView: View {
                     Button("\(Image(systemName: "info.circle"))") {
                         showingInfoView.toggle()
                     }
-                    .font(.title2)
-                    .sheet(isPresented: $showingInfoView) {
-                        InfoView()
-                    }
+                        .font(.title2)
+                        .sheet(isPresented: $showingInfoView) {
+                            InfoView()
+                        }
                 }
             }
             .foregroundColor(.red)
             .padding()
+            .onAppear {  // for first time run and UserDefaults not set
+                if lensFocalLength == 0 {
+                    lensFocalLength = 5
+                }
+                if upperFocalLimitSelection == 0 {
+                    upperFocalLimitSelection = 1
+                }
+                if cropFactor == 0 {
+                    cropFactor = 1.0
+                }
+                if ruleSelection == 0 {
+                    ruleSelection = 4
+                }
+            }
         }
     }
 }
--- Astro Max SUI/InfoView.swift
+++ Astro Max SUI/InfoView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct InfoView: View {
-    @Environment(\.dismiss) var dismiss
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         ZStack {
@@ -16,13 +16,13 @@ struct InfoView: View {
             VStack(spacing: 30) {
                 Text("Astrophotographers use the \"500 Rule\" as a way to estimate the maximum shutter speed that can be used to capture the night sky before star trailing becomes noticeable.\n\nDivide 500 (or a lower number rule) by the focal length of your full frame lens (crop factor of 1.0). For crop sensor cameras, the crop factor will be greater than 1.0 in order to scale or zoom up to a full frame equivalent size. Divide again by this value, which will reduce the usable shutter speed. (Crop factor of 1.5 for Nikon APS-C camera, 1.6 for Canon)")
                 Button("Done") {
-                    dismiss()
+                    presentationMode.wrappedValue.dismiss()
                 }
-                .font(.title2)
+                    .font(.title2)
             }
-            .font(.title3)
-            .foregroundColor(.red)
-            .padding()
+                .font(.title3)
+                .foregroundColor(.red)
+                .padding()
         }
     }
 }
